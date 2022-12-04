8241d1e53079a9765ff2f159ae8837c70dea0238
Shoubhit Dash <shoubhit2005@gmail.com>
2022-12-04 23:01:46 +0530

    not bad

---
+++ day-4/not-bad-1.ts
@@ -0,0 +1,23 @@
+export {};
+
+const input = await Deno.readTextFile("./input.txt").then((input) =>
+  input.split("\n")
+);
+
+console.log(
+  input
+    .map((pair) => pair.split(","))
+    .map((pair) => pair.map((range) => range.split("-")))
+    .map((pair) =>
+      pair.map((range) => [parseInt(range[0]), parseInt(range[1])])
+    )
+    .map((pair) =>
+      pair.map((range) => [Math.min(...range), Math.max(...range)])
+    )
+    .map(
+      (pair) =>
+        (pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]) ||
+        (pair[1][0] >= pair[0][0] && pair[1][1] <= pair[0][1])
+    )
+    .filter(Boolean).length
+);
