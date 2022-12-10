5fd84dd92c1f8a80531f5850f7f84ed04ad8dc7d
Shoubhit Dash <shoubhit2005@gmail.com>
2022-12-10 19:00:13 +0530

    change folder names to order them properly

--- setup.ts
+++ setup.ts
@@ -1,16 +1,14 @@
 import { format } from "https://deno.land/std@0.91.0/datetime/mod.ts";
 
-const day = format(new Date(), "d");
+const day = format(new Date(), "dd");
 console.log(day);
 
 const dir = `day-${day}`;
 
 const encoder = new TextEncoder();
-const data = encoder.encode(`export {}\n
-const input = await Deno.readTextFile("./input.txt");
-`);
 
 await Deno.mkdir(dir);
 await Deno.create(`./${dir}/input.txt`);
-await Deno.writeFile(`./${dir}/1.ts`, data);
-await Deno.writeFile(`./${dir}/2.ts`, data);
+await Deno.create(`./${dir}/test.txt`);
+await Deno.create(`./${dir}/1.py`);
+await Deno.create(`./${dir}/2.py`);
